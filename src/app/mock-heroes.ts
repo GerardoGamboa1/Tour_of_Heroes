d078ed415723b7106e0181d3573d6d15d592e5eb
GerardoGamboa1 <gerardoagm1902@gmail.com>
2022-07-10 17:47:03 -0300

    final producción web

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    { id: 1, name: 'Daredevil', power: 'Super sentidos' },
+    { id: 2, name: 'Moon Knight', power: 'Poder del dios egipcio de la luna' },
+    { id: 3, name: 'Iron Fist', power: 'Amplificación de fuerza a través del chi'},
+    { id: 4, name: 'Thor', power: 'Dios del trueno'},
+    { id: 5, name: 'Venom', power: 'Superfuerza y agilidad a través de simbiote extraterrestre' },
+    { id: 6, name: 'Star-Lord', power: 'Puede sobrevivir en el espacio exterior y crear campos de fuerza' },
+    { id: 7, name: 'Deadpool', power: 'Super regeneración e inmortalidad' },
+    { id: 8, name: 'Wolverine', power: 'Regeneración acelerada y super sentidos' },
+    { id: 9, name: 'Black Widow', power: 'Maestra en el espionaje, infiltración y artes marciales' },
+    { id: 10, name: 'The Wasp', power: 'Habilidad de encogerse y disparar rayos de energía' }
+];
